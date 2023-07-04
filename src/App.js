c889945bd08942fad1c0b8ac4bf096578e6a3da2
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-04 15:01:52 +0530

    created form custom component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, {useState} from "react";
 
 function App() {
+
+  const [query, setQuery] = useState("");
+
   return (
-    <>
-      <form className="search-form">
-        <input id="search" type="text" placeholder="Enter username or email" />
-        <button type="submit">Search</button>
-      </form>
-    </>
+    <div className="app">
+      <main className="main">
+        <h2>GitHub User Search</h2>
+        <Form
+          value={query}
+        />
+      </main>
+    </div>
+  );
+}
+function Form({ onSubmit, onChange, value }) {
+  return (
+    <form className="search-form" onSubmit={onSubmit}>
+      <input
+        id="search"
+        type="text"
+        placeholder="Enter username or email"
+        onChange={onChange}
+        value={value}
+      />
+      <button type="submit">Search</button>
+    </form>
   );
 }
 
