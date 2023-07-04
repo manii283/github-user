2e133902764454f823b8c96458c47561b45558ee
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-07-04 14:50:13 +0530

    created simple form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="search-form">
+        <input id="search" type="text" placeholder="Enter username or email" />
+        <button type="submit">Search</button>
+      </form>
+    </>
   );
 }
 
